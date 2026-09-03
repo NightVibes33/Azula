fd6b50b7568950ec6a44154d84de71cd6de0e4a3
Zyn <bobbytatum999@gmail.com>
2026-09-03 14:21:56 -0500

    Remove legacy fixed-window layout assumptions

--- Azula/AzulaApp.swift
+++ Azula/AzulaApp.swift
@@ -10,18 +10,8 @@ import SwiftUI
 @main
 struct AzulaApp: App {
     var body: some Scene {
-        #if os(macOS)
-        WindowGroup {
-            ContentView()
-                .frame(width: 300, height: 475)
-        }
-        .windowStyle(.hiddenTitleBar)
-        .windowResizability(.contentMinSize)
-        
-        #else
         WindowGroup {
             ContentView()
         }
-        #endif
     }
 }
